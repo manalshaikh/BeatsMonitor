ebae82ab9f7b0bcbe29279a3026f139b13b43a06
Manal Shaikh <shaikhmanal015@gmail.com>
2020-11-10 20:29:55 +0530

    Replaced pythonping with a function which I took from stackoverflow. I do not regret

--- monitor.py
+++ monitor.py
@@ -1,29 +1,51 @@
-from pythonping import ping # Required to be installed using `pip install pythingping` and only runs on root.
 from email.mime.text import MIMEText
 import smtplib
 from datetime import datetime
-ehost = 'mail.youremailserver.here' # Your mailserver hostname / IP address here.
-user = 'yourusername@mailprovider.here' # Your email ID / username for SMTP authentication here.
-password = 'YourpAsswordhEre' # Your password here.
+import time
+import platform    # For getting the operating system name
+import subprocess  # For executing a shell command
 
-ip = "125.33.68.2" # Test IP to deilberately show that the website is down.
-t = ping(ip)
-if t == True:
-    print("Site is online")
-else:
-    print("Site is offline")
-    sender = 'yourusername@mailprovider.here'
-    receiver = 'yourusername@gmail.com'
-    now = datetime.now()
-    msg = MIMEText('Your server was unpingable and assumed down at ' + now.strftime('%H:%M:%S - %d/%m/%Y.'))
+# Beginning of ping function
+def ping(host):
+    """
+    Returns True if host (str) responds to a ping request.
+    Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
+    """
 
-    msg['Subject'] = 'DOWN! Your website is down.' 
-    msg['From'] = user
-    msg['To'] = receiver
+    # Option for the number of packets as a function of
+    param = '-n' if platform.system().lower()=='windows' else '-c'
+    # Building the command. Ex: "ping -c 1 google.com"
+    command = ['ping', param, '1', host]
+
+    return subprocess.call(command, stdout=subprocess.DEVNULL) == 0
+# End of ping function
+
+ehost = 'buy.from.shadowhosting.net' # Your mailserver hostname / IP address here.
+user = 'buy@shadowhosting.net' # Your email ID / username for SMTP authentication here. (pls buy)
+password = 'plsbuyfromshadowhostingsothaticansupportthisproject' # Your password here.
 
 
-    with smtplib.SMTP("email.server.here", 587) as server:
-        server.starttls()
-        server.login(user, password)
-        server.sendmail(sender, receiver, msg.as_string())
-        print("mail successfully sent")
+
+def ChkOnline():
+    ip = "myserver.shadowhosting.net" # Test IP to deilberately show that the website is down.
+    t = ping(ip)
+    if t == True:
+        print("Site is online")
+    else:
+        print("Site is offline")
+        sender = user
+        receiver = 'selfmail@googlemailorwhateveryouwishhere.com' # Receiver's email where it is to be alerted.
+        now = datetime.now()
+        msg = MIMEText('Your server was unpingable and assumed down at ' + now.strftime('%H:%M:%S - %d/%m/%Y' '. We will check again after 5 minutes. '))
+
+        msg['Subject'] = 'DOWN! Your website is down.' 
+        msg['From'] = user
+        msg['To'] = receiver
+
+
+        with smtplib.SMTP(ehost, 587) as server:
+            server.starttls()
+            server.login(user, password)
+            server.sendmail(sender, receiver, msg.as_string())
+            print("mail successfully sent")  
+ChkOnline()
