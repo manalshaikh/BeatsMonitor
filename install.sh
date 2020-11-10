48e925e3c8e73aab7c034b2d8f8533781e9fd741
Manal Shaikh <shaikhmanal015@gmail.com>
2020-11-10 20:30:46 +0530

    Deleted mistake folder

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 yum -y install git
 yum -y install python3
 echo "================"
-echo "Successfully installed Git & Python. Beginning to install pythonping using pip3."
+echo "Successfully installed Git & Python. "
 echo "================"
 sleep 3
-pip3 install pythonping
 echo "================"
 echo "Now open and edit monitor.py with your SMTP details before executing cron.sh"
 echo "Make sure to edit beatsmon.cron with proper directory if not installed on /root/BeatsMonitor"
